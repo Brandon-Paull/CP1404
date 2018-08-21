15f16e416fd8d34450ab7e7c3a8f4c2f3db15ae5
Brandon Paull <jc462737@ad.jcu.edu.au>
2018-08-21 15:35:24 +1000

    Complete Prac 4 Required Code

--- Prac_04/lottery.py
+++ Prac_04/lottery.py
@@ -1,5 +1,26 @@
-NUMBERS_PER_LINE = 6
-MINIMUM = 45
-MAXIMUM = 1
 import random
-rounds = int(input("How many quick picks? "))
+
+NUMBERS_PER_LINE = 6
+MINIMUM = 1
+MAXIMUM = 45
+
+
+def main():
+    """Quick Picks - generate random numbers """
+    rounds = int(input("How many quick picks? "))
+    while rounds < 0:
+        print("Rounds must be greater than 0")
+        rounds = int(input("How many quick picks? "))
+
+    for i in range(rounds):
+        picks = []
+        for j in range(NUMBERS_PER_LINE):
+            number = random.randint(MINIMUM, MAXIMUM)
+            while number in picks:
+                number = random.randint(MINIMUM, MAXIMUM)
+            picks.append(number)
+        picks.sort()
+        print(" ".join("{:2}".format(number) for number in picks))
+
+
+main()
