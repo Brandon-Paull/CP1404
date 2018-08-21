169027ca32f5acf2bd85d728ef5502efee29252b
Brandon Paull <jc462737@ad.jcu.edu.au>
2018-08-21 14:19:48 +1000

    Add list_files.py to prac 3

--- Practicals/Prac_04/lists_warmup.py
+++ Practicals/Prac_04/lists_warmup.py
@@ -9,3 +9,14 @@ print(5 in numbers)  # True
 print(7 in numbers)  # False
 print("3" in numbers)  # False
 print(numbers + [6, 5, 3])  # [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
+
+numbers[0] = 10
+numbers[-1] = 1
+print(numbers[2::])
+
+if 9 in numbers:
+    print("True")
+else:
+    print("False")
+
+
